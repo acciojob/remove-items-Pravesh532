3bf5e676d953d177e4f955278dadabe44a24c35c
Pravesh532 <praveshpatel9569@gmail.com>
2025-03-23 14:57:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required. 
+function remove() {
+
+ const dropDown = documnet.getElementBtId("colorDropDown");
+	const selectIndex = dropDown.selectedIndex;
+	if(selectIndex !== -1){
+		selectIndex.remove();
+	}
+}
+ 
